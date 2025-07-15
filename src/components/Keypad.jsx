e3055bfd52fd47aed6bedf03134a6b991665f82e
Rjack00 <Rjackson77@comcast.net>
2025-07-15 17:16:43 -0600

    src/components/Keypad.jsx updated
    classNames and onClick keys/values added to buttons objects

--- src/components/Keypad.jsx
+++ src/components/Keypad.jsx
@@ -5,20 +5,20 @@ function Keypad({ onInput, onEquals }) {
         {id: "seven", label: "7" },
         {id: "eight", label: "8" },
         {id: "nine", label: "9" },
-        {id: "divide", label: "/" },
+        {id: "divide", label: "/", className: "op-btn" },
         {id: "four", label: "4" },
         {id: "five", label: "5" },
         {id: "six", label: "6" },
-        {id: "multiply", label: "x" },
+        {id: "multiply", label: "x", className: "op-btn" },
         {id: "one", label: "1" },
         {id: "two", label: "2" },
         {id: "three", label: "3" },
-        {id: "subtract", label: "-" },
-        {id: "zero", label: "0" },
+        {id: "subtract", label: "-", className: "op-btn" },
+        {id: "zero", label: "0", className: "span2 num-btn" },
         {id: "decimal", label: "." },
-        {id: "add", label: "+" },
+        {id: "add", label: "+", className: "op-btn" },
         {id: "clear", label: "C" },
-        {id: "equals", label: "=" }
+        {id: "equals", label: "=", className: "span3 op-btn", onClick: onEquals }
     ];
 
     return (
